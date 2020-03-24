0dd68d2da8ebd7a1b4fda8b1cdab4dc21578ada5
Storey Sheldon <storey.e.sheldon@gmail.com>
2020-03-24 17:53:09 -0500

    added headed, attempted sort

--- src/components/SortForm.js
+++ src/components/SortForm.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { Link, useLocation } from "react-router-dom";
-import { BrowserRouter as Router } from "react-router-dom";
 import "./css/SortForm.css";
 
 function SortForm(){
@@ -10,28 +9,26 @@ function SortForm(){
     return (
         <div className="jumbotron">
             <div className="container d-flex justify-content-around flex-wrap">
-                <Router>
-                    <Link to="/">
-                        <button className={location.pathname === "/" ? "btn btn-warning" : "btn btn-success"}>
+                    <Link to="/employee-directory">
+                        <button className={location.pathname === "/employee-directory" || location.pathname === "/employee-directory/" ? "btn btn-warning" : "btn btn-success"}>
                             Default
                         </button>
                     </Link>
-                    <Link to="/male">
-                        <button className={location.pathname === "/male" ? "btn btn-warning" : "btn btn-success"}>
+                    <Link to="/employee-directory/male">
+                        <button className={location.pathname === "/employee-directory/male" ? "btn btn-warning" : "btn btn-success"}>
                             Filter for Men
                         </button>
                     </Link>
-                    <Link to="/female">
-                        <button className={location.pathname === "/female" ? "btn btn-warning" : "btn btn-success"}>
+                    <Link to="/employee-directory/female">
+                        <button className={location.pathname === "/employee-directory/female" ? "btn btn-warning" : "btn btn-success"}>
                             Filter for Women
                         </button>
                     </Link>
-                    <Link to="/sort">
-                        <button className={location.pathname === "/sort" ? "btn btn-warning" : "btn btn-success"}>
+                    <Link to="/employee-directory/sort">
+                        <button className={location.pathname === "/employee-directory/sort" ? "btn btn-warning" : "btn btn-success"}>
                             Sort Alphabetically
                         </button>
                     </Link>
-                </Router>
             </div>
         </div>
     );
